b229a781cb7030e877b690437aafc77440776a05
mohamed <alxmohamedemadeldin@gmail.com>
2023-08-15 03:30:19 -0700

    edited

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -18,32 +18,34 @@
 void print_times_table(int num)
 {
 	int i, j, result;
-    if (num >= 0 && num <= 15)
+
+	if (num >= 0 && num <= 15)
 	{
-			for (i = 0; i <= num; i++)
-	{
-		_putchar('0');
-		for (j = 1; j <= num; j++)
+		for (i = 0; i <= num; i++)
 		{
-			_putchar(',');
-			_putchar(' ');
-			result = i * j;
-			if (result <= 99 && result <= 9)
+			_putchar('0');
+			for (j = 1; j <= num; j++)
 			{
+				_putchar(',');
 				_putchar(' ');
+				_putchar(' ');
+				result = i * j;
+				if (result <= 99 && result <= 9)
+				{
+					_putchar(' ');
+				}
+				if (result >= 100)
+				{
+					_putchar((result / 100) + '0');
+					_putchar(((result / 10)) % 10 + '0');
+				}
+				else if (result <= 99 && result >= 10)
+				{
+					_putchar((result / 10) + '0');
+				}
+				_putchar((result % 10) + '0');
 			}
-			if (result >= 100)
-			{
-				_putchar((result / 100) + '0');
-				_putchar(((result / 10)) % 10 + '0');
-			}
-			else if (result <= 99 && result >= 10)
-			{
-				_putchar((result / 10) + '0');
-			}
-			_putchar((result % 10) + '0');
+			_putchar('\n');
 		}
-		_putchar('\n');
-	}
 	}
 }
