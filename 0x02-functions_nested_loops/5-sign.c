8c62171f0d40b529af6586a2bb7d20579c1482c8
mohamed <alxmohamedemadeldin@gmail.com>
2023-08-15 02:19:18 -0700

    edited

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -11,7 +11,7 @@
  * Return: 1 if positive, -1 if negative, 0 if zero
  */
 
-int print_sign(num)
+int print_sign(int num)
 {
 	if (num > 0)
 	{
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -2,12 +2,12 @@ int _putchar(char name);
 int print_alphabet(void);
 int print_alphabet_x10(void);
 int _islower(int character);
-int _isalpha(int character);
-int print_sign(num);
-int _abs(num);
+int _isalpha(int character)
+int print_sign(int num);
+int _abs(int num);
 int print_last_digit(int num);
 int jack_bauer(void);
 int times_table(void);
-int add(num_one, num_two);
-int print_to_98(num);
-int print_times_table(num);
+int add(int num_one, int num_two);
+int print_to_98(int num);
+int print_times_table(int num);
