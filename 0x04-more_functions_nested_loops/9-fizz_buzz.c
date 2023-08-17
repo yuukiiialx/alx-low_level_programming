ea7d1cd13c74dc25a3887bdefd61236ab47fc2fb
mohamed <alxmohamedemadeldin@gmail.com>
2023-08-17 02:23:00 -0700

    Edited

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -7,7 +7,7 @@
  * Description: This program implements the FizzBuzz game.
  *           It prints numbers from 1 to 100,
  *			replacing numbers divisible by 3 with "Fizz",
- *		numbers divisible by 5 with "Buzz",
+ *  numbers divisible by 5 with "Buzz",
  *				and numbers divisible by both 3 and 5 with "FizzBuzz".
  *
  * Return: 0
@@ -16,23 +16,27 @@
 
 int main(void)
 {
-	for (int i = 1; i <= 100; i++)
+	int j = 1;
+
+	printf("%d", j);
+
+	for (int i = 2; i <= 100; i++)
 	{
 		if (i % 3 == 0 && i % 5 == 0)
 		{
-			printf("FizzBuzz\n");
+			printf(" FizzBuzz");
 		}
 		else if (i % 3 == 0)
 		{
-			printf("Fizz\n");
+			printf(" Fizz");
 		}
 		else if (i % 5 == 0)
 		{
-			printf("Buzz\n");
+			printf(" Buzz");
 		}
 		else
 		{
-			printf("%d\n", i);
+			printf(" %d", i);
 		}
 	}
 	printf("\n");
