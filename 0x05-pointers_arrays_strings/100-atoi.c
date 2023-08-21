7dde63de31846d438e6a8eb475b1a38f39bb1653
mohamed <alxmohamedemadeldin@gmail.com>
2023-08-20 21:16:38 -0700

    Edited

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -11,18 +11,27 @@
 int _atoi(char *s)
 {
 	int i = 0;
+	int sign = 1;
+	int result = 0;
+
+	if (s[0] == '-')
+	{
+		sign = -1;
+		i++;
+	}
 
 	while (s[i] != '\0')
 	{
-		if ((s[i] >= '0' && s[i] <= '9') ||
-		(s[i] == '-' && s[i + 1] >= '0' && s[i + 1] <= '9'))
+		if (s[i] >= '0' && s[i] <= '9')
 		{
-			_putchar(s[i]);
+			result = result * 10 + (s[i] - '0');
+		}
+		else
+		{
+			break;
 		}
 		i++;
 	}
 
-	_putchar('\n');
-
-	return (0);
+	return (result * sign);
 }
