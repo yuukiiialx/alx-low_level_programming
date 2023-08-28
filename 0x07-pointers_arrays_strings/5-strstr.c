e96bda20de80cf1ffed85b5a2e91e93cf6554d08
mohamed <alxmohamedemadeldin@gmail.com>
2023-08-28 02:04:28 -0700

    Edited

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -13,12 +13,14 @@
 
 char *_strstr(char *haystack, char *needle)
 {
+	int i;
+
 	if (*needle == 0)
 		return (haystack);
 
 	while (*haystack)
 	{
-		for (int i = 0; haystack[i] == needle[i]; i++)
+		for (i = 0; haystack[i] == needle[i]; i++)
 		{
 			if (needle[i + 1] == '\0')
 				return (haystack);
