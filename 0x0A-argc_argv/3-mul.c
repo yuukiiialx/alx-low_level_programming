dc8cf8403d9cfddd131bb1e53460a99ec322ad8e
mohamed <alxmohamedemadeldin@gmail.com>
2023-09-01 02:25:36 -0700

    Edited

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -24,11 +24,6 @@ int main(int argc, char *argv[])
 		return (1);
 	}
 
-	int num1 = atoi(argv[1]);
-	int num2 = atoi(argv[2]);
-	int result = num1 * num2;
-
-	printf("%d\n", result);
-
+	printf("%d\n", atoi(argv[1]) * atoi(argv[2]));
 	return (0);
 }
