95fe94d30728f0ba7d2b6c9b525de3beb06eceba
mohamed <alxmohamedemadeldin@gmail.com>
2023-09-03 21:02:41 -0700

    Solved

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -3,63 +3,43 @@
 #include "main.h"
 
 /**
- * str_length - Computes the length of a string.
- * @str: The string to compute the length of.
- *
- * Return: The length of the string.
- */
-int str_length(char *str)
-{
-	int length = 0;
-
-	while (str[length] != '\0')
-	{
-		length++;
-	}
-
-	return (length);
-}
-
-/**
  * str_concat - Concatenates two strings.
  * @s1: The first string.
  * @s2: The second string.
  *
- * Return: A pointer to a newly allocated string containing the concatenated
- *         s1 and s2, or NULL on failure.
+ * Return: A pointer to the concatenated string,
+ *		or NULL if memory allocation fails.
  */
+
 char *str_concat(char *s1, char *s2)
 {
+	char *con;
+	int i, j = 0,  length = 0;
+
 	if (s1 == NULL)
+	{
 		s1 = "";
+	}
 	if (s2 == NULL)
 		s2 = "";
 
-	int len1 = str_length(s1);
-	int len2 = str_length(s2);
-	int total_len = len1 + len2 + 1;
-	int i, j;
+	for (i = 0; s1[i] || s2[i]; i++)
+	{
+		length++;
+	}
+	con = malloc(sizeof(char) * length);
 
-	char *concat_str = malloc(total_len * sizeof(char));
-
-	if (concat_str == NULL)
+	if (con == NULL)
 	{
 		return (NULL);
 	}
-
-	for (i = 0; i < len1; i++)
+	for (i = 0; s1[i]; i++)
 	{
-		concat_str[i] = s1[i];
+		con[j++] = s1[i];
 	}
-
-	for (j = 0; j < len2; j++)
+	for (i = 0; s2[i]; i++)
 	{
-		concat_str[i] = s2[j];
-		i++;
+		con[j++] = s2[i];
 	}
-
-	concat_str[total_len - 1] = '\0';
-
-	return (concat_str);
+	return (con);
 }
-
