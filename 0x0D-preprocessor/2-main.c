f3820df2a32dbaa0f7494cb18fa05a8396b7aa40
mohamed <alxmohamedemadeldin@gmail.com>
2023-09-12 02:03:10 -0700

    Solved

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Entry point of the program
+ *
+ * Description: This program prints the name of the source file.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
+
