e043e07f80afbea6e555672e1d173fc70ea1ff79
Mohamed <alxmohamedemadeldin@gmail.com>
2024-02-21 06:27:14 -0800

    Solved

---
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+""" Island Perimeter"""
+
+
+def island_perimeter(grid):
+    """ Island Perimeter"""
+    count = 0
+    rows = len(grid)
+    cols = len(grid[0])
+
+    for i in range(rows):
+        for j in range(cols):
+            if grid[i][j] == 1:
+                # Count the number of land neighbors
+                neighbors = 0
+
+                if i > 0 and grid[i - 1][j] == 1:
+                    neighbors += 1
+                if i < rows - 1 and grid[i + 1][j] == 1:
+                    neighbors += 1
+                if j > 0 and grid[i][j - 1] == 1:
+                    neighbors += 1
+                if j < cols - 1 and grid[i][j + 1] == 1:
+                    neighbors += 1
+
+                # Add to the count based on the number of land neighbors
+                count += 4 - neighbors
+
+    return count
